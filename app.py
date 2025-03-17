4f07b75a796eae509946b4eceae184e30d709ac4
MANISH-Shetty-Repos <manishshettyoth9182@gmail.com>
2025-03-17 21:13:07 +0530

    make the app accesible for the ip address

--- app.py
+++ app.py
@@ -52,5 +52,8 @@ def analyze():
                            sentiment=sentiment_result['label'], 
                            emotion=emotion_result['label'])
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
